8926080e03ef932f7f59fab05daa715c9265688e
zzyy1128 <2282921521@qq.com>
2026-09-10 22:03:27 +0800

    chore: add project and Cypress configuration

---
+++ 3测试代码及产品源代码/be/cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+import dbPlugin from "./cypress/plugins";
+
+export default defineConfig({
+  projectId: 'g8j64j',
+  e2e: {
+    setupNodeEvents(on, config) {
+      return dbPlugin(on, config);
+    },
+    baseUrl: "http://localhost:8080",
+    supportFile: "cypress/support/e2e.ts",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    experimentalRunAllSpecs: true,
+  },
+  env: {
+    apiUrl: "http://localhost:8080/api",
+  },
+});
